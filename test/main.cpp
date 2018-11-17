b454c6a864da71f984e8b1a56058e8578c453843
Brandon Dyer <brandondyer64@gmail.com>
2018-11-17 13:16:27 -0600

    Vulkan window with GLFW

--- test/main.cpp
+++ test/main.cpp
@@ -1,11 +1,35 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
 #include <iostream>
-#include "Math/Vectors/Vec2.h"
 
 int main() {
-  Kepler::Vec2 a(1, 2);
-  Kepler::Vec2 b(2, 3);
-  Kepler::Vec2 c = a + b;
-  std::cout << c.x << std::endl;
-  std::cout << Kepler::TestFun() << std::endl;
-  return 0;
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    std::cout << extensionCount << " extensions supported" << std::endl;
+
+    glm::mat4 matrix;
+    glm::vec4 vec;
+    auto test = matrix * vec;
+
+    while(!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+
+    return 0;
 }
