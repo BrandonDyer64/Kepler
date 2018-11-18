7e7f3f2256238d9e7d68bff9896b8b19e4695499
Brandon Dyer <brandondyer64@gmail.com>
2018-11-18 00:22:45 -0600

    More Vulkan frameworking

---
+++ framework/Engine/DesktopApplication.h
@@ -0,0 +1,15 @@
+
+#ifndef DesktopApplication_h
+#define DesktopApplication_h
+
+#include "../World/Level.h"
+
+namespace Kepler {
+
+  class DesktopApplication {
+  public:
+    DesktopApplication();
+  };
+}
+
+#endif
--- framework/World/Actor.h
+++ framework/World/Actor.h
@@ -2,20 +2,15 @@
 #ifndef Actor_h
 #define Actor_h
 
+#include "../Math/Vectors/Vec3.h"
+#include "../Math/Vectors/Quaternion.h"
+
 namespace Kepler {
 
   class Actor {
   public:
-    const float x;
-    const float y;
-  public:
-    Vec2(float x, float y): x(x), y(y) {};
-    Vec2 operator+(const Vec2& other) const;
-    Vec2 operator-(const Vec2& other) const;
-    Vec2 operator*(const Vec2& other) const;
-    Vec2 operator/(const Vec2& other) const;
-    float Dot(const Vec2& other) const;
-    float DistanceTo(const Vec2& other) const;
+    Vec3 location;
+    Quaternion rotation;
   };
 }
 
