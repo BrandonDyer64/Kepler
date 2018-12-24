7fb480fbd4c1e5e407b8f51b4b869930dc7b83cc
Brandon Dyer <brandondyer64@gmail.com>
2018-12-24 10:34:37 -0600

    Third party

--- framework/Engine/Artemis/Entity.h
+++ framework/Engine/Artemis/Entity.h
@@ -1,89 +1,86 @@
 #ifndef ENTITY_H
 #define ENTITY_H
 
-#include <bitset>
-#include <string>
-#include <cstddef>
-#include <typeinfo>
 #include "BitSize.h"
-#include "ImmutableBag.h"
-#include "EntityManager.h"
 #include "ComponentTypeManager.h"
+#include "EntityManager.h"
+#include "ImmutableBag.h"
+#include <bitset>
+#include <cstddef>
+#include <string>
+#include <typeinfo>
 
 namespace Kepler {
 
-	class Component;
-	class ComponentType;
-	class World;
-	//class EntityManager;
+class Component;
+class ComponentType;
+class World;
+// class EntityManager;
 
-	/**
-   * The entity class. Cannot be instantiated outside the framework, you must
-   * create new entities using World.
-   */
-  class Entity {
+/**
+ * The entity class. Cannot be instantiated outside the framework, you must
+ * create new entities using World.
+ */
+class Entity {
 
-  private:
-    int id;
-    long int uniqueId;
-    std::bitset<BITSIZE> typeBits;
-    std::bitset<BITSIZE> systemBits;
-    World * world;
-    EntityManager * entityManager;
+private:
+  int id;
+  long int uniqueId;
+  std::bitset<BITSIZE> typeBits;
+  std::bitset<BITSIZE> systemBits;
 
-    // No copy constructor
-    Entity(const Entity&);
-    // No assign operator
-    Entity& operator=(const Entity&);
+  // No copy constructor
+  Entity(const Entity &);
+  // No assign operator
+  Entity &operator=(const Entity &);
 
-  protected:
+protected:
+  World *world;
+  EntityManager *entityManager;
 
-  public:
-    Entity(World * world, int id);
-    ~Entity();
-    int GetId();
-    void SetUniqueId(long int uniqueId);
-    long int getUniqueId();
+public:
+  Entity(World *world, int id);
+  ~Entity();
+  int GetId();
+  void SetUniqueId(long int uniqueId);
+  long int getUniqueId();
 
-    std::bitset<BITSIZE> getTypeBits();
-    void addTypeBit(std::bitset<BITSIZE> bit);
-    void removeTypeBit(std::bitset<BITSIZE> bit);
-    std::bitset<BITSIZE> GetSystemBits();
-    void addSystemBit(std::bitset<BITSIZE> bit);
-    void removeSystemBit(std::bitset<BITSIZE> bit);
-    void SetSystemBits(std::bitset<BITSIZE> systemBits);
-    void setTypeBits(std::bitset<BITSIZE> typeBits);
-    void reset();
+  std::bitset<BITSIZE> getTypeBits();
+  void addTypeBit(std::bitset<BITSIZE> bit);
+  void removeTypeBit(std::bitset<BITSIZE> bit);
+  std::bitset<BITSIZE> GetSystemBits();
+  void addSystemBit(std::bitset<BITSIZE> bit);
+  void removeSystemBit(std::bitset<BITSIZE> bit);
+  void SetSystemBits(std::bitset<BITSIZE> systemBits);
+  void setTypeBits(std::bitset<BITSIZE> typeBits);
+  void reset();
 
-    std::string toString();
+  std::string toString();
 
-    void AddComponent(Component * c);
+  void AddComponent(Component *c);
 
-    //Might change to non template
-    template<typename c>
-    void RemoveComponent() {
-      entityManager->RemoveComponent(*this,ComponentTypeManager::getTypeFor<c>());
-    }
+  // Might change to non template
+  template <typename c> void RemoveComponent() {
+    entityManager->RemoveComponent(*this,
+                                   ComponentTypeManager::getTypeFor<c>());
+  }
 
-    void RemoveComponent(ComponentType & type);
+  void RemoveComponent(ComponentType &type);
 
+  Component *getComponent(ComponentType &type);
 
-    Component * getComponent(ComponentType & type);
+  template <typename c> Component *getComponent() {
+    return (c *)entityManager->getComponent(
+        *this, ComponentTypeManager::getTypeFor<c>());
+  }
 
-    template<typename c>
-    Component * getComponent() {
-      return (c*)entityManager->getComponent(*this,ComponentTypeManager::getTypeFor<c>());
-    }
+  ImmutableBag<Component *> &getComponents();
 
-    ImmutableBag<Component*> & getComponents();
-
-    bool isActive();
-    void Refresh();
-    void remove();
-    void setGroup(std::string group);
-    void setTag(std::string tag);
-
-
-  };
+  bool isActive();
+  void Refresh();
+  void remove();
+  void setGroup(std::string group);
+  void setTag(std::string tag);
 };
+};     // namespace Kepler
 #endif // $(Guard token)
--- framework/ThirdParty/ThirdParty.h
+++ framework/ThirdParty/ThirdParty.h
@@ -1,4 +1,15 @@
+
 #if defined(LIB_KEPLER) && defined(__has_include)
+
+#pragma message ("Third Party")
+
+// libRocket
+#if __has_include("libRocket/Include/Rocket/Core.h")
+  #pragma message ("libRocket")
+  #include "libRocket/Include/Rocket/Core.h"
+  #include "libRocket/Include/Rocket/Controls.h"
+  #include "libRocket/Include/Rocket/Debugger.h"
+#endif
 
 // Bullet 3
 #if __has_include("bullet3/src/btBulletDynamicsCommon.h")
