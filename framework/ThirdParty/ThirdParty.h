9a8ae2cd991b93f007e8392dec47dc3adbde0722
Brandon Dyer <brandondyer64@gmail.com>
2018-12-24 21:04:16 -0600

    libRocket updates. still doesn't work

--- framework/ThirdParty/ThirdParty.h
+++ framework/ThirdParty/ThirdParty.h
@@ -3,22 +3,30 @@
 
 #pragma message ("Third Party")
 
-// libRocket
+// zlib - Asset compression
+#if __has_include("zlib/zlib.h")
+  #define USING_ZLIB
+  #include "zlib/zlib.h"
+#endif
+
+// libRocket - User Interface with HTML and CSS
 #if __has_include("libRocket/Include/Rocket/Core.h")
-  #pragma message ("libRocket")
+  #define USING_LIBROCKET
   #include "libRocket/Include/Rocket/Core.h"
   #include "libRocket/Include/Rocket/Controls.h"
   #include "libRocket/Include/Rocket/Debugger.h"
 #endif
 
-// Bullet 3
+// Bullet 3 - 3D Physics
 #if __has_include("bullet3/src/btBulletDynamicsCommon.h")
+  #define USING_BULLET3
   #include "bullet3/src/btBulletDynamicsCommon.h"
   #include "bullet3/src/btBulletCollisionCommon.h"
 #endif
 
-// Box2D
+// Box2D - 2D Physics
 #if __has_include("Box2D/Box2D/Box2D.h")
+  #define USING_BOX2D
   #include "Box2D/Box2D/Box2D.h"
 #endif
 
