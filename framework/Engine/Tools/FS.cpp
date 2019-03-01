cb7e11de227d8f22b82d108a0dd6ee91a058c708
Brandon Dyer <brandondyer64@gmail.com>
2019-03-01 17:16:42 -0600

    String split

--- framework/Engine/Tools/FS.cpp
+++ framework/Engine/Tools/FS.cpp
@@ -1,6 +1,7 @@
 
 #include "FS.h"
 #include <fstream>
+#include <string>
 #include <streambuf>
 #include <sstream>
 
@@ -11,6 +12,19 @@ std::string ReadFile(const std::string &filename) {
   std::stringstream buffer;
   buffer << t.rdbuf();
   return buffer.str();
+}
+
+std::vector<std::string> ReadFile(const std::string &filename, char delim) {
+  std::ifstream file("data/" + filename);
+  if (!file.is_open()) {
+    std::cout << "Failed to open file: " << filename << std::endl;
+  }
+  std::vector<std::string> out;
+  std::string line;
+  while (std::getline(file, line, delim)) {
+    out.push_back(line);
+  }
+  return out;
 }
 
 } // namespace Kepler
--- framework/Engine/Tools/FS.h
+++ framework/Engine/Tools/FS.h
@@ -9,6 +9,8 @@ namespace Kepler::FS {
 
 std::string ReadFile(const std::string &filename);
 
+std::vector<std::string> ReadFile(const std::string &filename, char delim);
+
 } // namespace Kepler
 
 #endif
