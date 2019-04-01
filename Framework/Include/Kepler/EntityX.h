7c520b8c84d98ee94350a66191d0738552a11f2a
Brandon Dyer <brandondyer64@gmail.com>
2019-04-01 13:36:42 -0500

    EntityX

---
+++ Framework/Include/Kepler/EntityX.h
@@ -0,0 +1,1 @@
+#include <entityx/quick.h>
---
+++ Framework/Include/Kepler/Math/Number.h
@@ -0,0 +1,11 @@
+#ifndef Number_h
+#define Number_h
+
+class Number {
+  long long integer;
+  float decimal;
+public:
+  Number(long long, float);
+};
+
+#endif
