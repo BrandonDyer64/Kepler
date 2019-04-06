c26c96e9e95bec1fc239f643024214e11b194cb4
Brandon Dyer <brandondyer64@gmail.com>
2019-04-05 22:50:42 -0500

    Added slight base for a tool bar

---
+++ editor/src/Frontend/Windows/Main/ToolBar/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './index.module.css'
+
+export default class ToolBar extends React.Component {
+  render() {
+    return (
+      <div className={styles.ToolBar}>
+        <span className='mdi mdi-package-variant-closed' />
+      </div>
+    )
+  }
+}
