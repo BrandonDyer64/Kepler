fc7b7951cf816bb53ca955100b54ccd6df57381a
Brandon Dyer <brandondyer64@gmail.com>
2019-04-11 16:41:19 -0500

    Editor API base

--- Editor/src/Frontend/Windows/Main/Editor/api.js
+++ Editor/src/Frontend/Windows/Main/Editor/api.js
@@ -1,9 +1,21 @@
+import _ from 'lodash'
+
 class API {
   constructor() {
-    this.editors = []
+    this.editors = {}
     this.subscriber = null
   }
-  addEditor(editor) {}
+  addEditor(name, editor) {
+    this.editors[name] = editor
+    this.update()
+  }
+  removeEditor(name) {
+    delete this.editors[name]
+    this.update()
+  }
+  update() {
+    this.subscriber.setState({ editors: this.editors })
+  }
   subscribe(subscriber) {
     this.subscriber = subscriber
   }
