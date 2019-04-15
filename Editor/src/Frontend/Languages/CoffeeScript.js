2f2edac170568cfce64814c4af9081969069cf8a
Brandon Dyer <brandondyer64@gmail.com>
2019-04-15 12:35:48 -0500

    Base for language parsing

---
+++ Editor/src/Frontend/Languages/CoffeeScript.js
@@ -0,0 +1,7 @@
+import coffeeScript from '../Parsers/ArrowChains'
+
+export default {
+  extensionsIn: ['cof', 'coffee'],
+  extensionOut: 'js',
+  parsers: [coffeeScript]
+}
---
+++ Editor/src/Frontend/Languages/index.js
@@ -0,0 +1,2 @@
+const array = [require('./CoffeeScript'), require('./JavaScript')]
+export default function p() {}
