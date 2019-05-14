951bb79b48853068e218f1b404581b8507cceab8
bdyer <bdyer-contractor@cars.com>
2019-05-14 15:34:26 -0500

    LIB-18 | testing glfw on ci

--- Display/src/kepler_display.cc
+++ Display/src/kepler_display.cc
@@ -1,18 +1,57 @@
 #include "display/Display.hpp"
 
 #include <napi.h>
+#include <sstream>
+#include <string>
+#include <iostream>
 
 using namespace Napi;
 
 Boolean Test_GlfwInit(const CallbackInfo& info) {
   Env env = info.Env();
   GLFWwindow *window;
-  return Boolean::New(env, glfwInit);
+  return Boolean::New(env, glfwInit());
+}
+
+Boolean Test_GlfwIncluded(const CallbackInfo& info) {
+  Env env = info.Env();
+  #ifdef _glfw3_h_
+    return Boolean::New(env, true);
+  #else
+    return Boolean::New(env, false);
+  #endif
+}
+
+Boolean Test_GladIncluded(const CallbackInfo& info) {
+  Env env = info.Env();
+  #ifdef __glad_h_
+    return Boolean::New(env, true);
+  #else
+    return Boolean::New(env, false);
+  #endif
+}
+
+String GlfwGetVersion(const CallbackInfo& info) {
+  Env env = info.Env();
+  int major = 0, minor = 0, rev = 0;
+  glfwGetVersion(&major, &minor, &rev);
+  std::ostringstream str;
+  str
+    << major << '.'
+    << minor << '.'
+    << rev;
+  return String::New(env, str.str());
 }
 
 Object Init(Env env, Object exports) {
   exports.Set(String::New(env, "Test_GlfwInit"),
               Function::New(env, Test_GlfwInit));
+  exports.Set(String::New(env, "Test_GlfwIncluded"),
+              Function::New(env, Test_GlfwIncluded));
+  exports.Set(String::New(env, "Test_GladIncluded"),
+              Function::New(env, Test_GladIncluded));
+  exports.Set(String::New(env, "GlfwGetVersion"),
+              Function::New(env, GlfwGetVersion));
   return exports;
 }
 
--- Display/test/binding.test.js
+++ Display/test/binding.test.js
@@ -1,8 +1,23 @@
-const { Test_GlfwInit } = require('../lib/binding.js')
+const {
+  Test_GlfwInit,
+  Test_GlfwIncluded,
+  Test_GladIncluded,
+  GlfwGetVersion
+} = require('../lib/binding.js')
 const assert = require('assert')
 
+console.log(GlfwGetVersion())
+
 describe('Kepler Display', function() {
+  describe('GLAD', function() {
+    it('should be included', function() {
+      assert.equal(Test_GladIncluded(), true)
+    })
+  })
   describe('GLFW', function() {
+    it('should be included', function() {
+      assert.equal(Test_GlfwIncluded(), true)
+    })
     it('should initialize', function() {
       assert.equal(Test_GlfwInit(), true)
     })
